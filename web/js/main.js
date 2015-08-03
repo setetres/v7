a8e2c911127ce37be6842cc3522df8d5c15f0d38
Guilherme Pangnotta <setetres@gmail.com>
2015-08-03 16:20:24 -0300

    Fix

--- web/js/main.js
+++ web/js/main.js
@@ -1,6 +1,38 @@
 $(document).ready(function() {
     'use strict';
-    $('.fit-video').fitVids();
+
+    var rotateDegree = 0;
+
+    function setSizes() {
+        $('.row, footer').css({height: window.innerHeight});
+        $('#content').css({top: window.innerHeight});
+        $('footer').css({marginBottom: window.innerHeight});
+    }
+
+    // window resize
+
     $(window).on('debouncedresize',function() {
+        setSizes();
     });
+
+    // mousewheel
+
+    $('body').on('mousewheel', function(event) {
+        if(event.deltaY < 0 && $(window).scrollTop() + window.innerHeight === $(document).height()){
+            $(window).scrollTop(0);
+            rotateDegree -= 360;
+            $('.logo').css({transform: 'perspective(1000px) rotateX('+rotateDegree+'deg)'});
+            //$('#audio-flip')[0].play();
+        } else if(event.deltaY > 0 && $(window).scrollTop() === 0){
+            $(window).scrollTop($(document).height() - window.innerHeight);
+            rotateDegree += 360;
+            $('.logo').css({transform: 'perspective(1000px) rotateX('+rotateDegree+'deg)'});
+            //$('#audio-flip')[0].play();
+        }
+    });
+
+    // init
+
+    setSizes();
+
 });
