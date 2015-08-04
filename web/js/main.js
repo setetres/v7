05e5c81b5d0a4eb09f9721f8b3d107dc9a0149c8
Guilherme Pangnotta <setetres@gmail.com>
2015-08-04 16:36:52 -0300

    Fix

--- web/js/main.js
+++ web/js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 
     var rotateDegree = 0;
 
+    // set sizes
+
     function setSizes() {
         $('.row, footer').css({height: window.innerHeight});
         $('#content').css({top: window.innerHeight});
@@ -19,16 +21,33 @@ $(document).ready(function() {
 
     $('body').on('mousewheel', function(event) {
         if(event.deltaY < 0 && $(window).scrollTop() + window.innerHeight === $(document).height()){
+            randomLanguage = contentTranslate[Math.floor((Math.random() * contentTranslate.length))];
             $(window).scrollTop(0);
             rotateDegree -= 360;
             $('.logo').css({transform: 'perspective(1000px) rotateX('+rotateDegree+'deg)'});
-            //$('#audio-flip')[0].play();
+            $('.logo a').text(randomLanguage[1]);
+            setTimeout(function(){
+                $('.description').html(randomLanguage[2]).css({transform: 'perspective(1000px) rotateX('+rotateDegree+'deg)'});
+            },200);
         } else if(event.deltaY > 0 && $(window).scrollTop() === 0){
+            randomLanguage = contentTranslate[Math.floor((Math.random() * contentTranslate.length))];
             $(window).scrollTop($(document).height() - window.innerHeight);
             rotateDegree += 360;
             $('.logo').css({transform: 'perspective(1000px) rotateX('+rotateDegree+'deg)'});
-            //$('#audio-flip')[0].play();
+            $('.logo a').text(randomLanguage[1]);
+            setTimeout(function(){
+                $('.description').html(randomLanguage[2]).css({transform: 'perspective(1000px) rotateX('+rotateDegree+'deg)'});
+            },200);
         }
+    });
+
+    // glitch
+
+    $('body').on('click', function() {
+        var glitchID = Math.floor((Math.random() * 255) + 1);
+        $('#glitch source').remove();
+        $('#glitch')[0].src = 'mp3/GL1-'+ glitchID +'.mp3';
+        $('#glitch')[0].play();
     });
 
     // init
