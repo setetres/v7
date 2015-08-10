39897dfc3d697cec755b3d4471313bca993997e7
Guilherme Pangnotta <setetres@gmail.com>
2015-08-10 10:43:50 -0300

    Fix

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,88 @@
+// var Sttrs = {
+//     isMobile : /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+//     setupUserTip : function(enable){
+//         if(enable) {
+//             Founders.userTipInterval = setInterval(function(){
+//                 $('.navigation-indicator').toggleClass('user-tip');
+//             },1500);
+//         } else {
+//             clearInterval(Founders.userTipInterval);
+//             $('.navigation-indicator').removeClass('user-tip');
+//         }
+//     },
+//     Navigation : {
+//         slideAnimationTime : 900,
+//         mouseWheelInterval : navigator.userAgent.match(/Macintosh/) ? 1200 : 400,
+//         lastMouseWheelAction : 0,
+//         selectedSlide : {
+//             x : 0,
+//             y : 0
+//         },
+//         zoomIn : function() {
+
+//         },
+//         zoomOut : function() {
+
+//         },
+//         onZoomIn : function() {
+
+//         },
+//         onZoomOut : function() {
+
+//         },
+//         setSelectedSlide : function(x, y) {
+
+//         },
+//         initializeMenu : function() {
+
+//         },
+//         updateMenu : function() {
+
+//         },
+//         keyboardObserver : function() {
+//         }
+//     },
+//     setSizes : function() {
+
+//     },
+//     loaded : function() {
+//     },
+//     reload : function() {
+//         var iScroll = Founders.DragManager.iScroll;
+//         for (var i = 0; i < iScroll.length; i++) {
+//             iScroll[i].refresh();
+//         }
+//     },
+//     onDocumentReady : function() {
+//         // if(Founders.isMobile) {
+//         //     $('.modal').css({display: 'block'});
+//         // }
+//         // $('body').on('click', '.bt-zoom.out', function(event) {
+//         //     if(!Founders.isMobile) {
+//         //         Founders.Navigation.zoomOut();
+//         //     } else {
+//         //         $('.modal').addClass('active');
+//         //     }
+//         //     event.preventDefault();
+//         // }).on('click', '.bt-zoom.in', function(event) {
+//         //     Founders.Navigation.zoomIn();
+//         //     event.preventDefault();
+//     }
+// };
+// $(document).ready(Founders.onDocumentReady);
+// $(window).on('load', Founders.loaded);
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
     'use strict';
 
@@ -6,37 +91,51 @@ $(document).ready(function() {
     // set sizes
 
     function setSizes() {
-        $('#home, footer').css({height: window.innerHeight});
-        $('#content').css({top: window.innerHeight});
+        $('#home, footer').css({
+            height: window.innerHeight
+        });
+        $('#content').css({
+            top: window.innerHeight
+        });
     }
 
     // window resize
 
-    $(window).on('debouncedresize',function() {
+    $(window).on('debouncedresize', function() {
         setSizes();
     });
+
+    setSizes();
 
     // mousewheel
 
     $('body').on('mousewheel', function(event) {
-        if(event.deltaY < 0 && $(window).scrollTop() + window.innerHeight === $(document).height()){
+        if (event.deltaY < 0 && $(window).scrollTop() + window.innerHeight === $(document).height()) {
             randomLanguage = contentTranslate[Math.floor((Math.random() * contentTranslate.length))];
             $(window).scrollTop(0);
             rotateDegree -= 360;
-            $('.logo').css({transform: 'perspective(1000px) rotateX('+rotateDegree+'deg)'});
+            $('.logo').css({
+                transform: 'perspective(1000px) rotateX(' + rotateDegree + 'deg)'
+            });
             $('.logo a').text(randomLanguage[2]);
-            setTimeout(function(){
-                $('.description').html(randomLanguage[3]).css({transform: 'perspective(1000px) rotateX('+rotateDegree+'deg)'});
-            },200);
-        } else if(event.deltaY > 0 && $(window).scrollTop() === 0){
+            setTimeout(function() {
+                $('.description').html(randomLanguage[3]).css({
+                    transform: 'perspective(1000px) rotateX(' + rotateDegree + 'deg)'
+                });
+            }, 200);
+        } else if (event.deltaY > 0 && $(window).scrollTop() === 0) {
             randomLanguage = contentTranslate[Math.floor((Math.random() * contentTranslate.length))];
             $(window).scrollTop($(document).height() - window.innerHeight);
             rotateDegree += 360;
-            $('.logo').css({transform: 'perspective(1000px) rotateX('+rotateDegree+'deg)'});
+            $('.logo').css({
+                transform: 'perspective(1000px) rotateX(' + rotateDegree + 'deg)'
+            });
             $('.logo a').text(randomLanguage[2]);
-            setTimeout(function(){
-                $('.description').html(randomLanguage[3]).css({transform: 'perspective(1000px) rotateX('+rotateDegree+'deg)'});
-            },200);
+            setTimeout(function() {
+                $('.description').html(randomLanguage[3]).css({
+                    transform: 'perspective(1000px) rotateX(' + rotateDegree + 'deg)'
+                });
+            }, 200);
         }
     });
 
@@ -49,23 +148,19 @@ $(document).ready(function() {
             filename = '0' + filename;
         }
         $('#container').addClass('glitch');
-        setTimeout(function(){
+        setTimeout(function() {
             $('#container').removeClass('glitch');
-        },150);
+        }, 150);
         $('#glitch source').remove();
-        $('#glitch')[0].src = 'mp3/glitch-'+ filename +'.mp3';
+        $('#glitch')[0].src = 'mp3/glitch-' + filename + '.mp3';
         $('#glitch')[0].play();
     });
 
-    // init
-
-    setSizes();
-
-    var getMax = function(){
+    var getMax = function() {
         return $(document).height() - $(window).height();
     };
 
-    var getValue = function(){
+    var getValue = function() {
         return $(window).scrollTop();
     };
 
@@ -73,20 +168,273 @@ $(document).ready(function() {
         max = getMax(),
         value, width;
 
-    var getWidth = function(){
+    var getWidth = function() {
         value = getValue();
-        width = (value/max) * 100;
+        width = (value / max) * 100;
         width = width + '%';
         return width;
     };
 
-    var setWidth = function(){
-        progressBar.css({ width: getWidth() });
+    var setWidth = function() {
+        progressBar.css({
+            width: getWidth()
+        });
     };
 
     $(document).on('scroll', setWidth);
-    $(window).on('resize', function(){
+    $(window).on('resize', function() {
         max = getMax();
         setWidth();
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (!Detector.webgl) {
+    Detector.addGetWebGLMessage();
+}
+var margin = 0;
+var time = 0;
+var screenWidth = window.innerWidth;
+var screenHeight = window.innerHeight;
+var clock = new THREE.Clock();
+var ambientLight,
+    camera,
+    composer,
+    container,
+    controls,
+    renderTarget,
+    current_material,
+    effect,
+    effectController,
+    hblur,
+    light,
+    material,
+    materials,
+    numBlobs,
+    pointLight,
+    renderer,
+    resolution,
+    scene,
+    effectFXAA,
+    vblur;
+
+function onWindowResize(event) {
+    'use strict';
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight - 2 * margin;
+    camera.aspect = screenWidth / screenHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(screenWidth, screenHeight);
+    composer.setSize(screenWidth, screenHeight);
+    hblur.uniforms.h.value = 4 / screenWidth;
+    vblur.uniforms.v.value = 4 / screenHeight;
+    effectFXAA.uniforms.resolution.value.set(1 / screenWidth, 1 / screenHeight);
+}
+
+function createShaderMaterial(id, light, ambientLight) {
+    'use strict';
+    var shader = THREE.ShaderToon[id];
+    var u = THREE.UniformsUtils.clone(shader.uniforms);
+    var vs = shader.vertexShader;
+    var fs = shader.fragmentShader;
+    var material = new THREE.ShaderMaterial({
+        uniforms: u,
+        vertexShader: vs,
+        fragmentShader: fs
+    });
+    material.uniforms.uDirLightPos.value = light.position;
+    material.uniforms.uDirLightColor.value = light.color;
+    material.uniforms.uAmbientLightColor.value = ambientLight.color;
+    return material;
+}
+
+function generateMaterials() {
+    'use strict';
+    // toons
+    var hatchingMaterial = createShaderMaterial('hatching', light, ambientLight);
+
+    var materials = {
+        'hatching': {
+            m: hatchingMaterial,
+            h: 0.2,
+            s: 1,
+            l: 0.9
+        }
+    };
+    return materials;
+}
+
+
+
+function setupGui() {
+    'use strict';
+    effectController = {
+        speed: 0.73,
+        numBlobs: 27,
+        resolution: 27,
+        isolation: 73,
+        hex: '0x4b538b',
+        lhue: 0.04,
+        lsaturation: 1.0,
+        llightness: 0.5,
+        lx: -1,
+        ly: 1,
+        lz: 1,
+        postprocessing: false,
+        dummy: function() {}
+    };
+}
+
+// this controls content of marching cubes voxel field
+function updateCubes(object, time, numblobs) {
+    'use strict';
+    object.reset();
+    // fill the field with some metaballs
+    var i, ballx, bally, ballz, subtract, strength;
+    subtract = 12;
+    strength = 1.2 / ((Math.sqrt(numblobs) - 1) / 4 + 1);
+    for (i = 0; i < numblobs; i++) {
+        ballx = Math.sin(i + 1.26 * time * (1.03 + 0.5 * Math.cos(0.21 * i))) * 0.27 + 0.5;
+        bally = Math.abs(Math.cos(i + 1.12 * time * Math.cos(1.22 + 0.1424 * i))) * 0.77; // dip into the floor
+        ballz = Math.cos(i + 1.32 * time * 0.1 * Math.sin((0.92 + 0.53 * i))) * 0.27 + 0.5;
+        object.addBall(ballx, bally, ballz, strength, subtract);
+    }
+}
+
+
+
+function render() {
+    'use strict';
+    var delta = clock.getDelta();
+    time += delta * effectController.speed * 0.5;
+    // marching cubes
+    if (effectController.resolution !== resolution) {
+        resolution = effectController.resolution;
+        effect.init(resolution);
+    }
+    if (effectController.isolation !== effect.isolation) {
+        effect.isolation = effectController.isolation;
+    }
+    updateCubes(effect, time, effectController.numBlobs);
+    // materials
+    if (effect.material instanceof THREE.ShaderMaterial) {
+        effect.material.uniforms.uBaseColor.value.setHex(effectController.hex);
+    } else {
+        effect.material.color.setHex(effectController.hex);
+    }
+    // lights
+    light.position.set(effectController.lx, effectController.ly, effectController.lz);
+    light.position.normalize();
+    // render
+    renderer.clear();
+    renderer.render(scene, camera);
+}
+
+function animate() {
+    'use strict';
+    requestAnimationFrame(animate);
+    render();
+}
+
+
+function init() {
+    'use strict';
+    container = document.getElementById('container');
+    // CAMERA
+    camera = new THREE.PerspectiveCamera(45, screenWidth / screenHeight, 1, 10000);
+    camera.position.set(-500, 500, 1500);
+    // SCENE
+    scene = new THREE.Scene();
+    // LIGHTS
+    light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(0.5, 0.5, 1);
+    scene.add(light);
+    ambientLight = new THREE.AmbientLight(0x080808);
+    scene.add(ambientLight);
+    // MATERIALS
+    materials = generateMaterials();
+    current_material = 'hatching';
+    // MARCHING CUBES
+    resolution = 28;
+    numBlobs = 10;
+    effect = new THREE.MarchingCubes(resolution, materials[current_material].m, true, true);
+    effect.position.set(0, 0, 0);
+    effect.scale.set(700, 700, 700);
+    effect.enableUvs = false;
+    effect.enableColors = false;
+    scene.add(effect);
+    // RENDERER
+    renderer = new THREE.WebGLRenderer({
+        alpha: true
+    });
+    renderer.setClearColor(0x15191d, 0);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(screenWidth, screenHeight);
+    renderer.domElement.style.position = 'absolute';
+    renderer.domElement.style.top = margin + 'px';
+    renderer.domElement.style.left = '0px';
+    container.appendChild(renderer.domElement);
+    //
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
+    // CONTROLS
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.noZoom = true;
+    // COMPOSER
+    renderer.autoClear = false;
+    var renderTargetParameters = {
+        minFilter: THREE.LinearFilter,
+        magFilter: THREE.LinearFilter,
+        format: THREE.RGBFormat,
+        stencilBuffer: false
+    };
+    renderTarget = new THREE.WebGLRenderTarget(screenWidth, screenHeight, renderTargetParameters);
+    effectFXAA = new THREE.ShaderPass(THREE.FXAAShader);
+    hblur = new THREE.ShaderPass(THREE.HorizontalTiltShiftShader);
+    vblur = new THREE.ShaderPass(THREE.VerticalTiltShiftShader);
+    var bluriness = 8;
+    hblur.uniforms.h.value = bluriness / screenWidth;
+    vblur.uniforms.v.value = bluriness / screenHeight;
+    hblur.uniforms.r.value = vblur.uniforms.r.value = 0.5;
+    effectFXAA.uniforms.resolution.value.set(1 / screenWidth, 1 / screenHeight);
+    composer = new THREE.EffectComposer(renderer, renderTarget);
+    var renderModel = new THREE.RenderPass(scene, camera);
+    vblur.renderToScreen = true;
+    //effectFXAA.renderToScreen = true;
+    composer = new THREE.EffectComposer(renderer, renderTarget);
+    composer.addPass(renderModel);
+    composer.addPass(effectFXAA);
+    composer.addPass(hblur);
+    composer.addPass(vblur);
+    // GUI
+    setupGui();
+    // EVENTS
+    window.addEventListener('resize', onWindowResize, false);
+}
+
+
+
+
+init();
+animate();
